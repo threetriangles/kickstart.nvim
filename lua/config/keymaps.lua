56540a7cea656ac591fbf64e53cc1349f09f585a
Russell Baker <russell@russellbaker.me>
2023-07-13 08:08:30 -0400

    add some documentation

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -13,10 +13,10 @@ vim.g.VtrPercentage = 50
 -- NvimTree
 vim.keymap.set({ 'n' }, '<leader>f', ':NvimTreeToggle<CR>', { silent = true })
 vim.keymap.set({ 'n' }, "<leader>ff", ':NvimTreeFindFile<CR>', { silent = true })
-vim.keymap.set({ 'n' }, '<leader>u', ':only<CR>', { silent = true })
+vim.keymap.set({ 'n' }, '<leader>u', ':only<CR>', { desc = 'Close other buffers' })
 
 -- copy file path to clipboard
-vim.keymap.set({ 'n' }, '<leader>cp', ':let @+ = expand("%")<CR>', { silent = true })
+vim.keymap.set({ 'n' }, '<leader>cp', ':let @+ = expand("%")<CR>', { desc = 'copy file path to clipboard' })
 
 -- escape and save all
 vim.keymap.set({ 'i' }, 'jj', '<ESC>:wall<CR>:echo "SAVED-ALL"<CR>', { silent = true })
